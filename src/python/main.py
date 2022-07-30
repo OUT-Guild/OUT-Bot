d58029bfd47b8c2ee0df321614fbddf429fa1689
StarbuckBarista <carlitos.salazar@protonmail.com>
2022-07-29 22:22:00 -0500

    Deleted Test Code
    There was some test code that has remained on the bot.

--- src/python/main.py
+++ src/python/main.py
@@ -13,6 +13,4 @@ if __name__ == "__main__":
                      mongodb_uri=getenv("MONGODB_URI"), hypixel_key=getenv("HYPIXEL_KEY"))
     client.run(getenv("BOT_TOKEN"))
 
-self.client.get_database_collection("users")
-user_collection.update_many({}, {"$unset": {"lastMessage": 1, "votes": 1, "timesSaid": 1, "characterCountStarted": 1, "characters": 1, "lastMuteEvent": 1, "username": 1}, "$rename": {"eventsWon": "events"}, "$set": {"uuid": None, "emote": None}}, {"multi": True})
 await ctx.send("Whew!")

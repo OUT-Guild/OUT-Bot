b31b7af2b2c84349fa104be46607f210af7f6fe7
StarbuckBarista <carlitos.salazar@protonmail.com>
2023-08-16 10:12:24 -0500

    Revived Collages
    *this was a mistake*

--- src/python/cogs/miscellaneous.py
+++ src/python/cogs/miscellaneous.py
@@ -5,6 +5,8 @@ from datetime import datetime, timedelta
 import discord
 from discord.ext import commands
 from json import load
+from random import choice
+from string import ascii_uppercase
 from traceback import format_exception
 
 import config
@@ -148,6 +150,177 @@ class Miscellaneous(commands.Cog):
 
         user_collection.update_one({"_id": member.id}, {"$inc": {"outcoins": 15000}})
         return await channel.send("Enjoy the OUT Coins :D")
+    
+    viewcollage_details = command_details["viewcollage"]
+
+    @commands.command(
+        name="viewcollage",
+        aliases=viewcollage_details["aliases"],
+        usage=viewcollage_details["usage"],
+        description=viewcollage_details["description"],
+        signature=viewcollage_details["signature"])
+    @commands.has_any_role(*viewcollage_details["required_roles"])
+    @commands.cooldown(viewcollage_details["cooldown_rate"], viewcollage_details["cooldown_per"])
+    async def viewcollage(self, ctx, member: discord.Member):
+        collage_collection = self.client.get_database_collection("collages")
+        collages = collage_collection.find({"member": member.id})
+
+        if collages.count() == 0:
+            clean_collage_embed = self.client.create_embed(
+                "Clean Collage",
+                f"{member.mention} has no embarrassing images to showcase.",
+                config.embed_success_color
+            )
+
+            return await ctx.reply(embed=clean_collage_embed)
+        
+        loading_data_embed = self.client.create_embed(
+            "Loading Data",
+            "Currently fetching data from our database...",
+            config.embed_info_color
+        )
+
+        page_index = 0
+        message = await ctx.reply(embed=loading_data_embed)
+
+        while True:
+            collage = collages[page_index]
+
+            collage_embed = self.client.create_embed(
+                f"{member.display_name}'s Collage",
+                "*embarrassingggg...*",
+                config.embed_success_color
+            )
+
+            collage_embed.add_field(name="Submitted By", value=f"<@!{collage['submitter']}>", inline=True)
+            collage_embed.add_field(name="Collage Image ID", value=collage["_id"], inline=True)
+            collage_embed.set_image(url=collage["image"])
+            collage_embed.set_footer(text=f"Image {page_index + 1}/{collages.count()}")
+
+            await message.edit(embed=collage_embed)
+            await message.add_reaction("⏮")
+            await message.add_reaction("⬅")
+            await message.add_reaction("🛑")
+            await message.add_reaction("➡")
+            await message.add_reaction("⏭")
+
+            collage_reply = await self.client.message_reaction(message, ctx.author, 150)
+
+            if collage_reply is None:
+                return
+
+            async def invalid_response():
+                invalid_response_embed = self.client.create_embed(
+                    "Invalid Response",
+                    "The response that you provided to the question was not acceptable.",
+                    config.embed_error_color
+                )
+
+                await message.edit(embed=invalid_response_embed)
+
+            if collage_reply not in ["⏮", "⬅", "🛑", "➡", "⏭"]:
+                return await invalid_response()
+
+            await message.remove_reaction(collage_reply, ctx.author)
+
+            if collage_reply == "⏮":
+                page_index = 0
+            elif collage_reply == "⬅":
+                page_index -= 1
+            elif collage_reply == "🛑":
+                await message.remove_reaction("⏮", ctx.guild.me)
+                await message.remove_reaction("⬅", ctx.guild.me)
+                await message.remove_reaction("🛑", ctx.guild.me)
+                await message.remove_reaction("➡", ctx.guild.me)
+                await message.remove_reaction("⏭", ctx.guild.me)
+                return
+            elif collage_reply == "➡":
+                page_index += 1
+            elif collage_reply == "⏭":
+                page_index = collages.count() - 1
+
+            if page_index == -1:
+                page_index = collages.count() - 1
+
+            if page_index == collages.count():
+                page_index = 0
+
+    addcollage_details = command_details["addcollage"]
+
+    @commands.command(
+        name="addcollage",
+        aliases=addcollage_details["aliases"],
+        usage=addcollage_details["usage"],
+        description=addcollage_details["description"],
+        signature=addcollage_details["signature"])
+    @commands.has_any_role(*addcollage_details["required_roles"])
+    @commands.cooldown(addcollage_details["cooldown_rate"], addcollage_details["cooldown_per"])
+    async def addcollage(self, ctx, member: discord.Member):
+        if len(ctx.message.attachments) == 0:
+            no_attachment_embed = self.client.create_embed(
+                "No Attachment",
+                "You did not attach an image to your message.",
+                config.embed_error_color
+            )
+
+            return await ctx.reply(embed=no_attachment_embed)
+        
+        collage_collection = self.client.get_database_collection("collages")
+
+        for attachment in ctx.message.attachments:
+            while True:
+                collage_id = "".join([choice(ascii_uppercase) for _ in range(6)])
+
+                if collage_collection.find({"_id": collage_id}).count() == 0:
+                    collage_collection.insert_one({
+                        "_id": collage_id,
+                        "member": member.id,
+                        "submitter": ctx.author.id,
+                        "image": attachment.url
+                    })
+
+                    break
+
+        collage_added_embed = self.client.create_embed(
+            "Collage Added",
+            f"Successfully added {len(ctx.message.attachments)} image(s) to {member.mention}'s collage.",
+            config.embed_success_color
+        )
+
+        return await ctx.reply(embed=collage_added_embed)
+    
+    removecollage_details = command_details["removecollage"]
+
+    @commands.command(
+        name="removecollage",
+        aliases=removecollage_details["aliases"],
+        usage=removecollage_details["usage"],
+        description=removecollage_details["description"],
+        signature=removecollage_details["signature"])
+    @commands.has_any_role(*removecollage_details["required_roles"])
+    @commands.cooldown(removecollage_details["cooldown_rate"], removecollage_details["cooldown_per"])
+    async def removecollage(self, ctx, collage_id: str):
+        collage_collection = self.client.get_database_collection("collages")
+        collage = collage_collection.find_one({"_id": collage_id})
+
+        if collage is None:
+            invalid_collage_embed = self.client.create_embed(
+                "Invalid Collage",
+                "The collage ID that you provided is invalid.",
+                config.embed_error_color
+            )
+
+            return await ctx.reply(embed=invalid_collage_embed)
+        
+        collage_collection.delete_one({"_id": collage_id})
+
+        collage_removed_embed = self.client.create_embed(
+            "Collage Removed",
+            f"Successfully removed {collage_id} from {collage['member']}'s collage.",
+            config.embed_success_color
+        )
+
+        return await ctx.reply(embed=collage_removed_embed)
 
 async def setup(client):
     await client.add_cog(Miscellaneous(client), guilds=[discord.Object(id=503560012581568514)])
